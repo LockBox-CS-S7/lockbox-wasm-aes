bb1672f84f00e6b1c62418216ae18738b4677048
rikdgd <rik.dgd@gmail.com>
2025-06-25 17:05:10 +0200

    Refactor encryption functions to return EncryptionResult; update HTML demo for file input handling and runtime display.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod aes;
 
+use std::time::Instant;
 use wasm_bindgen::prelude::*;
 use aes::aes_encryption::{encrypt, decrypt, EncryptedData};
 
@@ -14,12 +15,49 @@ pub fn greet(name: &str) {
 }
 
 #[wasm_bindgen]
-pub fn encrypt_bytes(bytes: Vec<u8>, password: &str) -> Vec<u8> {
-    encrypt(&bytes, password).expect("Failed to encrypt the received data")
+pub fn encrypt_bytes(bytes: Vec<u8>, password: &str) -> EncryptionResult {
+    let now = Instant::now();
+    let ciphertext = encrypt(&bytes, password).expect("Failed to encrypt the received data");
+    
+    EncryptionResult {
+        data: ciphertext,
+        duration: now.elapsed().as_millis(),
+    }
 }
 
 #[wasm_bindgen]
-pub fn decrypt_bytes(bytes: Vec<u8>, password: &str) -> Vec<u8> {
+pub fn decrypt_bytes(bytes: Vec<u8>, password: &str) -> EncryptionResult {
+    let now = Instant::now();
+    
     let data = EncryptedData::from_bytes(bytes);
-    decrypt(data, password).expect("Failed to decrypt the ciphertext")
+    let plaintext = decrypt(data, password).expect("Failed to decrypt the ciphertext");
+    
+    EncryptionResult { 
+        data: plaintext, 
+        duration: now.elapsed().as_millis() 
+    }
+}
+
+#[wasm_bindgen]
+pub struct EncryptionResult {
+    data: Vec<u8>,
+    duration: u128,
+}
+
+#[wasm_bindgen]
+impl EncryptionResult {
+    #[wasm_bindgen(constructor)]
+    pub fn new(data: Vec<u8>, duration: u128) -> EncryptionResult {
+        EncryptionResult { data, duration }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn data(&self) -> Vec<u8> {
+        self.data.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn duration(&self) -> u128 {
+        self.duration
+    }
 }
